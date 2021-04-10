ba278ab8ce4fff18db6e82e8412c2338de296bd2
Anthony Benjamin <landdeld@live.com>
2021-04-09 21:26:01 -0500

    implements section 1 of guide

--- src/app/heros/heros.component.ts
+++ src/app/heros/heros.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heros',
@@ -7,10 +8,14 @@ import { Hero } from '../hero';
   styleUrls: ['./heros.component.css'],
 })
 export class HerosComponent implements OnInit {
-  hero: Hero = {
-    id: 1,
-    name: 'Windstorm'
-  };
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+    //for testing only
+    //alert("I have clicked " + hero.name);
+  }
+
+  heroes = HEROES;
 
   constructor() { }
 
